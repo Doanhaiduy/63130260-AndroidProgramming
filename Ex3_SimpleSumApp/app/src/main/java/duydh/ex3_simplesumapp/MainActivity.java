175f1cc427c81c283e666204f392e6c1d253e84d
Doanhaiduy <haiduytbt2k3@gmail.com>
2024-03-11 21:53:19 +0700

    push Ex4_AddSubMulDiv_Onclick

--- Ex3_SimpleSumApp/app/src/main/java/duydh/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/duydh/ex3_simplesumapp/MainActivity.java
@@ -3,6 +3,8 @@ package duydh.ex3_simplesumapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void XuLyCong(View view) {
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKQ);
+
+        String strA = editTextSoA.getText().toString();
+        String strB = editTextSoB.getText().toString();
+
+        int a = Integer.parseInt(strA);
+        int b = Integer.parseInt(strB);
+        int ketQua = a + b;
+          String strKetQua = String.valueOf(ketQua);
+        editTextKetQua.setText(strKetQua);
     }
 }
