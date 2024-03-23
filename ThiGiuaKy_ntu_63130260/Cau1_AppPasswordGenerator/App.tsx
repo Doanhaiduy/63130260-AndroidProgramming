ca883dae5c1019cbf28539887a95c853d0b10b24
Doanhaiduy <haiduytbt2k3@gmail.com>
2024-03-23 23:29:46 +0700

    create screen, setup app.tsx

--- ThiGiuaKy_ntu_63130260/Cau1_AppPasswordGenerator/App.tsx
+++ ThiGiuaKy_ntu_63130260/Cau1_AppPasswordGenerator/App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import PasswordGenerator from './src/screens/PasswordGenerator';
 
 export default function App() {
     return (
         <View style={styles.container}>
             <StatusBar style='auto' />
+            <PasswordGenerator />
         </View>
     );
 }
---
+++ ThiGiuaKy_ntu_63130260/Cau1_AppPasswordGenerator/src/screens/PasswordGenerator.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const PasswordGenerator = (props: Props) => {
+    return (
+        <View>
+            <Text>PasswordGenerator</Text>
+        </View>
+    );
+};
+
+export default PasswordGenerator;
+
+const styles = StyleSheet.create({});
