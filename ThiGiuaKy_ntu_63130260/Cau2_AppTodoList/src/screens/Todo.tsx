619f9d8cd45b7fefd5800ebb50b80e5adfc007ff
Doanhaiduy <haiduytbt2k3@gmail.com>
2024-03-24 10:46:15 +0700

    handle add todo, import Dialog

--- ThiGiuaKy_ntu_63130260/Cau2_AppTodoList/src/screens/Todo.tsx
+++ ThiGiuaKy_ntu_63130260/Cau2_AppTodoList/src/screens/Todo.tsx
@@ -1,22 +1,32 @@
-import { FlatList, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Alert, FlatList, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import { AntDesign, Entypo } from '@expo/vector-icons';
 import { useDispatch, useSelector } from 'react-redux';
 import { addTodo, deleteTodo, toggleTodo } from '../store/todoSlice';
+import Dialog from 'react-native-dialog';
 
 type Props = {};
 
 const Todo = (props: Props) => {
     const { todos } = useSelector((state: any) => state.todo);
+    const [visible, setVisible] = React.useState(false);
+    const [todoName, setTodoName] = React.useState('');
     const dispatch = useDispatch();
 
-    const handleAddTodo = () => {
-        dispatch(
-            addTodo({
-                title: 'New Task',
-                completed: false,
-            })
-        );
+    const handleAddTodo = (name: string) => {
+        if (name.trim() === '') {
+            Alert.alert('Error', 'Please enter a valid task name');
+        } else {
+            dispatch(
+                addTodo({
+                    title: name,
+                    completed: false,
+                    id: Math.floor(Math.random() * 1000) + 1,
+                })
+            );
+            setTodoName('');
+            setVisible(false);
+        }
     };
 
     const handleDeleteTodo = (id: number) => {
@@ -28,9 +38,9 @@ const Todo = (props: Props) => {
     };
 
     return (
-        <SafeAreaView className='flex-1 bg-white'>
-            <Text className='text-3xl font-bold text-center text-[#032f54]'>Todo List</Text>
-            <ScrollView className='bg-[#f8f8fa] w-[85%] mx-auto mt-4 p-5 py-8 rounded-xl min-h-[70vh]'>
+        <SafeAreaView className='flex-1 bg-white '>
+            <Text className='text-3xl font-bold text-center text-[#032f54] mt-5'>Todo List</Text>
+            <View className='bg-[#f8f8fa] w-[85%] mx-auto mt-4 p-5 py-8 rounded-xl min-h-[70vh]'>
                 <FlatList
                     data={todos}
                     renderItem={({ item }) => {
@@ -80,12 +90,24 @@ const Todo = (props: Props) => {
                     }}
                     keyExtractor={(item) => item.id.toString()}
                 />
-            </ScrollView>
-            <TouchableOpacity className='w-[85%] mx-auto bg-[#032f54] rounded-full items-center flex-row p-2 justify-between mt-10'>
+            </View>
+            <TouchableOpacity
+                onPress={() => setVisible(true)}
+                className='w-[85%] mx-auto bg-[#032f54] rounded-full items-center flex-row p-2 justify-between mt-10'
+            >
                 <AntDesign name='pluscircleo' size={24} color='transparent' className='' />
                 <Text className='text-white text-lg font-semibold '>Add New Task</Text>
                 <AntDesign name='pluscircle' size={40} color='white' />
             </TouchableOpacity>
+            <View>
+                <Dialog.Container visible={visible}>
+                    <Dialog.Title>Add New Task</Dialog.Title>
+                    <Dialog.Description>Enter the name of the task you want to add to the list.</Dialog.Description>
+                    <Dialog.Input value={todoName} onChangeText={setTodoName} label='' />
+                    <Dialog.Button label='Cancel' onPress={() => setVisible(false)} />
+                    <Dialog.Button label='Add' onPress={() => handleAddTodo(todoName)} />
+                </Dialog.Container>
+            </View>
         </SafeAreaView>
     );
 };
