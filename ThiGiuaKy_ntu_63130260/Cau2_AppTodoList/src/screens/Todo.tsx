45b0a7e8d8fa2515cf381607d668aebebb74fde5
Doanhaiduy <haiduytbt2k3@gmail.com>
2024-03-24 09:25:58 +0700

    add, setup redux-toolkit

---
+++ ThiGiuaKy_ntu_63130260/Cau2_AppTodoList/src/screens/Todo.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const Todo = (props: Props) => {
+    return (
+        <View>
+            <Text>Todo</Text>
+        </View>
+    );
+};
+
+export default Todo;
+
+const styles = StyleSheet.create({});
